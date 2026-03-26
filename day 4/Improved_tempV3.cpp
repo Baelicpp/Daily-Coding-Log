e2e936b01b560808910a49e42ced3cc18e1a3f02
Baelicpp <bael1b0p3050@gmail.com>
2026-03-26 23:11:01 +0000

    i started from the beginning
    i make the code a-bit cleaner but started again because i was getting to many errors related to the "doubles" but today i learnt how to get the user input

---
+++ day 4/Improved_tempV3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+int x;
+    std::cout << "what temp value would you like to convert too?: \n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> x;
+    std::cout << "what temp value are you wanting to covert from?:\n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> x;
+} 
+

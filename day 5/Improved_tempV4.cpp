e98a893681d5905628bc720285aaf8a69170ebae
Baelicpp <bael1b0p3050@gmail.com>
2026-03-27 16:42:51 +0000

    alot of errors
    i spend most of my time debugging because of the new "if" statments but i can tell im getting closer and closer each day

---
+++ day 5/Improved_tempV4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+int baseunit;
+int target;
+int inputValue;
+
+double tempinput;
+double resultTemp;
+    std::cout << "what temp value are you wanting to covert from?:\n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> target;
+        std::cout << "Enter the temperature value: \n";
+std::cin >> inputValue;
+    std::cout << "what temp value would you like to convert too?: \n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> baseunit;
+
+
+if (baseunit == 1) {
+}
+    if (target == 2) {
+        // calculate from c to f
+    }
+    else if (target == 3) {
+        // calculate from c to k
+    }
+}
+
+// now i need to add the maths

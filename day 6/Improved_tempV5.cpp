ed1dcafc72ca6c0fe60b963446f7918d0affc9f5
Baelicpp <bael1b0p3050@gmail.com>
2026-03-28 22:26:56 +0000

    day 6
    today i only was able to get the maths i needed, because i keept getting an error in the terminal

---
+++ day 6/Improved_tempV5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+int baseunit;
+int target;
+int inputValue;
+
+double tempinput;
+double resultTemp;
+    std::cout << "what temp value are you wanting to covert from?:\n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> target;
+        std::cout << "Enter the temperature value: \n";
+std::cin >> inputValue;
+    std::cout << "what temp value would you like to convert too?: \n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> baseunit;
+
+
+if (baseunit == 1) {
+}
+    if (target == 2) {
+        // calculate from c to f
+    }
+    else if (target == 3) {
+        // calculate from c to k
+    }
+}
+
+
+celsius == (fahrenheit - 32)/1.8;
+fahrenheit == (kelvin + 273.15)/1.8;
+kelvin == (celsius - 32 * 5/9)/1.8;
+// now i need to add the maths

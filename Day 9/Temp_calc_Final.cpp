548feab0ca254551797d30222d98be8e1ed00dcd
Baelicpp <bael1b0p3050@gmail.com>
2026-03-31 21:37:45 +0100

    the final Result!!!!!
    everything works, i fixed all the bugs and made it so if the user puts an invalid input like "#" instead of a number the output is 0 no matter what, this is the final day working on this and I'm going to focus on more cyber security related things and reading leancpp.com

---
+++ Day 9/Temp_calc_Final.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+int main() {
+int baseunit;
+int target;
+
+double tempInCelsius;
+double inputValue;
+double tempinput = 0.0;
+double resultTemp = 0.0;
+                                                                                    
+std::cout << "What unit are you converting from? (1-3): \n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+
+std::cin >> target;
+
+std::cout << "Enter the temperature value: \n";
+std::cin >> inputValue;
+
+    std::cout << "what temp value would you like to convert too?: \n";
+    std::cout << "celsuis - 1 \n";
+    std::cout << "fahrenheit - 2 \n";
+    std::cout << "Kelvin - 3 \n";
+std::cin >> baseunit;
+
+if (target == 1) {
+    tempInCelsius = inputValue;
+} else if (target == 2) { // this is used to see if the user picked fahrenhiet
+    tempInCelsius = (inputValue - 32) /1.8; // this gets to results by getting the input value
+} else if (target == 3) {
+    tempInCelsius = inputValue - 273.15; // this converts it into kelvin 
+}
+
+if (baseunit == 1) {
+    resultTemp = tempInCelsius;
+}
+else if (baseunit == 2) {
+    resultTemp = (tempInCelsius * 1.8) + 32;
+}
+else if (baseunit == 3) {
+    resultTemp = tempInCelsius + 273.15;
+}
+std::cout << "final result: " << resultTemp << std::endl;
+return 0;
+}

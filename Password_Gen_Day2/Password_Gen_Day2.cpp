6dd86ce14e84c159639aeb788f4c8a17adf5309e
Baelicpp <bael1b0p3050@gmail.com>
2026-04-02 11:43:49 +0100

    Password gen day 2
    this project definetly isnt going to take as much time as the last but im taking my time learning all the new concepts like "for loops" and strings.

---
+++ Password_Gen_Day2/Password_Gen_Day2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+int password;
+int main() {
+
+std::string pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+std::string length = "";
+std::cout <<"Please enter a password length to generate 1-50: ";
+std::cin >> length;
+
+for (int i = 0; i < 50; i++){
+}
+
+std::cout << length;
+}

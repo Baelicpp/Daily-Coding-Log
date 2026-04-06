239cbfe8d4bbc5c74db515218bfa9ebb05fabab8
Baelicpp <bael1b0p3050@gmail.com>
2026-04-06 21:47:32 +0100

    Simple ATM project
    i want to do this project because I'm not very confident in for loops and i want to master the basics before i move on to the harder things

---
+++ ATM_Day1/ATM_Day1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+double balance;
+int us;
+int main() {
+std::cout << "please select an option:\n";
+std::cout << "(1) Check Balance: \n";
+std::cout << "(2) Deposit: \n";
+std::cout << "(3) Withdraw: \n";
+std::cin >> us;
+
+
+if (us == 3){
+    std::cout << "How much do you want to withdraw: ";
+}
+else (us == 2) ;{
+    std::cout << "how much would you like to deposit:";
+}
+}

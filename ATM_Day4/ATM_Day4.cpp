617fc077ffa7da600d0a7bd0fde6ce8749be0b9d
Baelicpp <bael1b0p3050@gmail.com>
2026-04-09 22:58:29 +0100

    ATM DAY 4
    today i implemented a while loop and fixed some bugs this project is almost done and i will probably have it done by tomorrow so I'm going to move onto a harder project

---
+++ ATM_Day4/ATM_Day4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+double deposit;
+double withdraw;
+double balance = 177.54;
+double input;
+int us;
+
+int main() {
+
+while (us != 4) { 
+std::cout << "\nplease select an option:\n";
+std::cout << "(1) Check Balance: \n";
+std::cout << "(2) Deposit: \n";
+std::cout << "(3) Withdraw: \n";
+std::cout <<"(4) Exit\n";
+std::cin >> us;
+
+
+
+
+if (us == 1) {
+    std::cout << "Your balance is: $" << balance;
+}
+else if(us == 2) {
+    std::cout << "how much would you like to deposit: ";
+    std::cin >> input;
+ balance = (balance + input);
+ std::cout << "Your balanced has increased too: $" << balance;
+}
+else if(us == 3) {
+    std::cout << "How much do you want to withdraw: \n";
+    std::cin >> input;
+if (input <= balance){
+    withdraw = (balance - input);
+std::cout << "Amount left in balance: $ \n" << withdraw;
+}
+else {
+    std::cout <<"Insufficient funds! \n";
+
+}
+std::cout <<"\nIf you want to go back press (4)\n";
+std::cin >> us;
+}
+}
+}

863f4b456d950b9510aee0af891aec1ff5bcc226
Baelicpp <bael1b0p3050@gmail.com>
2026-04-11 21:41:10 +0100

    Vault day 1
    today i wanted to make and start a longer program so i am making a bank login in screen, then I'm going to crack it with brute force and then I'm going to "patch" it so they no longer work

---
+++ Vault_Day1/Vault_Day1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int user;
+int pass;
+int main() {
+
+std::string username = "1234";
+std::string password = "1";
+
+if (username == 1) {
+    std::cout << "please enter your user name: ";
+    std::cin >> user;
+}
+else if (password == 2) {
+    std::cout << "please enter your password: ";
+    std::cin >> pass;
+}
+}]]]]
